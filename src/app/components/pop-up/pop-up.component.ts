d3d2346939f5bb5fef4a2e5570c299a169d48ccb
nixoletas <nicholetas@gmail.com>
2024-08-06 10:59:19 -0400

    fix: pop-up abrindo de novo quando clica no link

--- src/app/components/pop-up/pop-up.component.ts
+++ src/app/components/pop-up/pop-up.component.ts
@@ -34,6 +34,7 @@ export class PopUpComponent implements OnInit, OnDestroy {
     if (isModalShown) {
       this.isVisible = false;
     } else {
+
       this.isVisible = true;
       sessionStorage.setItem('isModalShown', 'true');
       // Automatically close the modal after a set time, if configured
@@ -69,12 +70,6 @@ export class PopUpComponent implements OnInit, OnDestroy {
 
   closeModal(): void {
     this.isVisible = false;
-  }
-
-  handleLinkClick(event: MouseEvent): void {
-    // Prevents the default link action to avoid reopening the modal
-    event.stopImmediatePropagation();
-    this.closeModal();
   }
 
   ngOnDestroy(): void {
