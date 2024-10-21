3a9441aa5545264ba3d2fe83d92b6e3ca75aaff5
nixoletas <nicholetas@gmail.com>
2024-10-21 15:58:48 -0400

    add export PATH

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ cd /home/sti/intranet-gitea
 
 git pull
 
-/home/sti/.local/state/fnm_multishells/166162_1729539262280/bin/npm run build
+export PATH=/home/sti/.local/state/fnm_multishells/166162_1729539262280/bin:$PATH
+
+npm run build
 
 docker compose down -v && docker compose up -d
