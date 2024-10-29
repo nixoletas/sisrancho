34c6de34e37b5cb51b4994f848ece8b1a90ac549
nixoletas <nicholetas@gmail.com>
2024-10-29 17:07:51 -0400

    tudo certo com componente de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { FormsModule, NgModel, NgModelGroup } from '@angular/forms';
 
 @Component({
   selector: 'br-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  cpf: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin(): void {
+    this.authService.login(this.cpf, this.password).subscribe({
+      next: (response) => {
+        console.log('Login bem-sucedido', response);
+        this.router.navigate(['/dashboard']); // Redireciona para a página protegida
+      },
+      error: (error) => {
+        console.error('Erro de login', error);
+        alert('Erro de login! Verifique suas credenciais.');
+      }
+    });
+  }
 
 }
