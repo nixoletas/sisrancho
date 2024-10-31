8dfffd373973a0cec8d0ca1594da34b7e66ad344
Nicholas_Miyasato <nicholetas@gmail.com>
2024-10-31 08:13:03 -0400

    trabalhando na funcionalidade do arranchamento

--- angular/src/app/pages/dashboard/dashboard.component.ts
+++ angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
-import { Component, NgModule, NgModuleRef, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { ArranchamentoService } from '../../arranchamento.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'br-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   providers: [NgModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
@@ -13,8 +15,13 @@ export class DashboardComponent implements OnInit {
   userCpf: string | null = '';
   userPg: string | null = '';
   userNomeCompleto: string | null = '';
+  arranchamentos: any[] = [];
+  selectedDate: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private arranchamentoService: ArranchamentoService
+  ) {}
 
   ngOnInit(): void {
     this.authService.getUserData().subscribe({
@@ -27,6 +34,45 @@ export class DashboardComponent implements OnInit {
         console.error('Erro ao buscar dados do usuário:', err);
       }
     });
+  }
+
+  loadArranchamentos(cpf: string): void {
+    this.arranchamentoService.getArranchamentos(cpf).subscribe((data) => {
+      this.arranchamentos = data;
+    });
+  }
+
+  onDateSelect(event: Event) {
+    const input = event.target as HTMLInputElement; // Cast para HTMLInputElement
+    const selectedDate = input.valueAsDate;
+    if (selectedDate) {
+      console.log(selectedDate); // Lógica para manipular a data
+    }
+  }
+
+  onDateChange(event: Event) {
+    const input = event.target as HTMLInputElement; // Especifica o tipo
+    const selectedDate = input.valueAsDate;
+    if (selectedDate) {
+      // Lógica para quando a data está definida
+      console.log(selectedDate);
+    }
+  }
+
+  addArranchamento(): void {
+    if (this.userCpf && this.selectedDate) {
+      this.arranchamentoService.addArranchamento(this.userCpf, this.selectedDate).subscribe(() => {
+        this.loadArranchamentos(this.userCpf!);
+      });
+    }
+  }
+
+  cancelArranchamento(date: string): void {
+    if (this.userCpf) {
+      this.arranchamentoService.cancelArranchamento(this.userCpf, date).subscribe(() => {
+        this.loadArranchamentos(this.userCpf!);
+      });
+    }
   }
 
 }
